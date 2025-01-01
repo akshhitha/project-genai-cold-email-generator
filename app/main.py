c64a2ff6d2f3029dbd6ebd5211b9dc57e7d18a68
akshhitha <111169379+akshhitha@users.noreply.github.com>
2025-01-01 19:38:36 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
 from chains import Chain
-from portfolio import Portfolio
+#from portfolio import Portfolio
 from utils import clean_text
 
 
@@ -28,7 +28,7 @@ def create_streamlit_app(llm, portfolio, clean_text):
 
 if __name__ == "__main__":
     chain = Chain()
-    portfolio = Portfolio()
+    #portfolio = Portfolio()
     st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
     create_streamlit_app(chain, portfolio, clean_text)
 
